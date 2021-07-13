d9bf100d6a012fae5bafb8cf17573b055211dfc0
Cleyton Caminha <cleytoncaminha01@gmail.com>
2021-07-13 17:31:43 -0300

    studyReact

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,13 +2,12 @@ import '../styles/dashboard.scss';
 import '../styles/result.scss'
 import React from 'react';
 import { Tips } from './Tips';
-import { Result } from './Result';
+
 
 export function Dashboard(){
     return(
-        <div className="dashboard">
+        <div>
          <Tips></Tips>
-         <Result></Result>
         </div>
     )
 }
--- src/components/Tips.js
+++ src/components/Tips.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import '../styles/dashboard.scss';
+import '../styles/result.scss';
 
-function targetValue(event){
-    console.log(event.target.value)
-
-}
 
 export function Tips(){
+   function billValue(){
+        let t = document.getElementsByClassName("billvalue")
+        console.log(t)
+   }
+
     return (
-        <div>
-            <div className="bill"><p>Bill</p>
-         <div><input type="number" placeholder="0"></input></div>
+        <div className="dashboard">
+         <div>  <div className="bill"><p>Bill</p>
+         <div><input type="number" className="billvalue" placeholder="0" onChange={billValue}></input></div>
          </div>
         <div className="tips"><p>select tips %</p>
         <div className="btns">
-        <button type="submit" onClick={targetValue} className="btn" value="5" >5%</button>
-        <button type="submit" onClick={targetValue} className="btn" value="10">10%</button>
-        <button type="submit" onClick={targetValue} className="btn" value="15">15%</button>
-        <button type="submit" onClick={targetValue} className="btn" value="25">25%</button>
-        <button type="submit" onClick={targetValue} className="btn" value="50">50%</button>
+        <button type="submit" className="btn" value="5" >5%</button>
+        <button type="submit" className="btn" value="10">10%</button>
+        <button type="submit" className="btn" value="15">15%</button>
+        <button type="submit"  className="btn" value="25">25%</button>
+        <button type="submit" className="btn" value="50">50%</button>
         <input type="number" placeholder="Custom"></input></div>
         </div>
-        <div className="people">
-            <p>Number of People</p>
-            <input type="number" placeholder="0"></input>
-        </div></div>
+        <div className="people"> <div className="numberpeople">
+            <p>Number of People</p> <p className="error">Can't be zero</p></div>
+            <input type="number" className="inputerror" placeholder="0"></input>
+        </div>
+        </div>
+
+        <div className="results">
+
+        <div className="amount">
+            <div>Tip amount <br/><span>/ person</span></div>
+        <div class="number"> 52,00</div>
+        </div>
+        <div className="amount total"><div>Total <br/><span>/ person</span></div>
+        <div class="number"> 52,00</div> </div>
+        <div className="reset"><button value="RESET">RESET</button></div>
+       
+    </div>
+    
+    </div>
     )
 }
